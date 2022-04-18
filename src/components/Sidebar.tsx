8b7e3157a2e36824f24869f5f24fbd6dc0be6149
Gabriel Santos <gabrielbarretos2000@outlook.com>
2022-04-18 21:29:31 +0000

    create user page created

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,14 +15,14 @@ export function Sidebar() {
             GERAL
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" alignItems="center">
+            <Link href="/dashboard" display="flex" alignItems="center">
               <Icon as={RiDashboardLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Dashboard
               </Text>
             </Link>
 
-            <Link display="flex" alignItems="center">
+            <Link href="/users" display="flex" alignItems="center">
               <Icon as={RiContactsLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Usuários
